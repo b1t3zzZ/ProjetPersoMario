f59691ed9fa44586608b1530fe658cc437cd478e
b1t3zzZ <tsibulevskiym@gmail.com>
2025-04-10 16:30:13 +0200

    asd

--- Code/js/app.js
+++ Code/js/app.js
@@ -1,10 +1,141 @@
-const levelMap = [`
-    ......................
-    ..#................#..
-    ..#..............=.#..
-    ..#.........o.o....#..
-    ..#.@......#####...#..
-    ..#####............#..
-    ......#++++++++++++#..
-    ......##############..
-    ......................`];
+window.myGamePiece = null;
+window.allComponents = [];
+
+function createComponents() {
+    const components = [];
+
+    const levelMap = [
+        ".............................................................",
+        ".............................................................",
+        ".............................................................",
+        ".................==..........................................",
+        ".............................................................",
+        ".............................................................",
+        ".............................................................",
+        ".............................................................",
+        "................=OO=.........................................",
+        ".............................................................",
+        ".............................................................",
+        ".............................................................",
+        "#...........#....==....#.....................................",
+        "#..........##....==....##....................................",
+        "#.........###..........###...................................",
+        "#........####..........####..................................",
+        "#.@.....#####.^......^.#####......<...#......................",
+        "#######################################################......",
+    ];
+
+    for (let row = 0; row < levelMap.length; row++) {
+        for (let col = 0; col < levelMap[row].length; col++) {
+            const char = levelMap[row][col];
+            let x = col * 40;
+            let y = row * 40;
+
+            switch (char) {
+                case '#':
+                    components.push(new component(40, 40, './images/terraMario.png', x, y));
+                    break;
+                case '=':
+                    components.push(new component(40, 40, './images/blockMario.png', x, y));
+                    break;
+                case '@':
+                    myGamePiece = new component(35,55, "./images/playerMario.png", x, y);
+                    break;
+                case 'O':
+                    components.push(new component(40, 40, './images/luckyMario.png', x, y));
+                    break;
+                case '^':
+                    components.push(new component(35, 35, './images/mushroomMario.png', x, y));
+                    break;
+                case '<':
+                    components.push(new component(35, 35, './images/mushroomMario.png', x, y));
+                    break;
+            }
+        }
+    }
+
+    return components;
+}
+
+function component(width, height, imageSrc, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0.5;
+    this.gravitySpeed = 0;
+    this.jumpStrenght = -13;
+    this.isJumping = false;
+
+    this.update = function () {
+        if (this.image.complete) {
+            myGameArea.context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    };
+
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.gravitySpeed += this.gravity;
+        this.y += this.speedY + this.gravitySpeed;
+    };
+
+    this.hitSides = function () {
+        const rockbottom = myGameArea.canvas.height - this.height;
+        const rocksides = myGameArea.canvas.width - this.width;
+
+        if (this.x > rocksides) this.x = rocksides;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
+            this.isJumping = false;
+        }
+        if (this.x < 0) this.x = 0;
+    };
+
+    this.jump = function () {
+        const marioJump = new Audio("./sounds/mario-jump.mp3");
+        marioJump.volume = 0.08;
+
+        if (!this.isJumping) {
+            this.gravitySpeed = this.jumpStrenght;
+            this.isJumping = true;
+
+            if (!marioJump.paused) {
+                marioJump.currentTime = 0;
+            }
+            marioJump.play();
+        }
+    };
+}
+
+window.updateGameArea = function () {
+    myGameArea.clear();
+
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+
+    if (myGameArea.keys && myGameArea.keys[65]) myGamePiece.speedX = -4;
+    if (myGameArea.keys && myGameArea.keys[68]) myGamePiece.speedX = 4;
+    if (myGameArea.keys && myGameArea.keys[87]) myGamePiece.jump();
+    if (myGameArea.keys[65] && myGameArea.keys[68]) myGamePiece.speedX = 0;
+
+    allComponents.forEach((comp) => {
+        comp.update();
+        comp.hitSides();
+    });
+
+    myGamePiece.update();
+    myGamePiece.newPos();
+    myGamePiece.hitSides();
+};
+
+function startGame() {
+    window.allComponents = createComponents();
+    myGameArea.start();
+}
+
+startGame();
--- Code/js/canvas.js
+++ Code/js/canvas.js
@@ -1,96 +1,22 @@
-let myGamePiece; // Игрок
-let allComponents = []; // Все компоненты игры
-
-// Создание компонентов
-function createComponents() {
-    const components = [];
-
-    const levelMap = [
-        ".............................................................",
-        ".............................................................",
-        ".............................................................",
-        ".................==..........................................",
-        ".............................................................",
-        ".............................................................",
-        ".............................................................",
-        ".............................................................",
-        "................=OO=.........................................",
-        ".............................................................",
-        ".............................................................",
-        ".............................................................",
-        "#...........#....==....#.....................................",
-        "#..........##....==....##....................................",
-        "#.........###..........###...................................",
-        "#........####..........####..................................",
-        "#.@.....#####.^......^.#####......<...#......................",
-        "#######################################################......",
-    ];
-
-    // Перебор карты уровня
-    for (let row = 0; row < levelMap.length; row++) {
-        for (let col = 0; col < levelMap[row].length; col++) {
-            const char = levelMap[row][col];
-            let x = col * 40; // Умножаем на 40, чтобы компоненты были выровнены по сетке
-            let y = row * 40;
-
-            switch (char) {
-                case '#':
-                    components.push(new component(40, 40, './images/terraMario.png', x, y));
-                    break;
-                case '=':
-                    components.push(new component(40, 40, './images/blockMario.png', x, y));
-                    break;
-                case '@':
-                    myGamePiece = new component(35, 55, './images/playerMario.png', x, y);  // Игрок может иметь немного другие размеры
-                    break;
-                case 'O':
-                    components.push(new component(40, 40, './images/luckyMario.png', x, y));
-                    break;
-                case '^':
-                    components.push(new component(35, 35, './images/mushroomMario.png', x, y));
-                    break;
-                case '<':
-                    components.push(new component(35, 35, './images/mushroomMario.png', x, y));
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    return components;
-}
-
-// Запуск игры
-function startGame() {
-    allComponents = createComponents(); // Теперь создаем компоненты перед началом игры
-    myGameArea.start();
-}
-
-// Canvas окно
-const myGameArea = {
+window.myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
         this.canvas.id = "canvasStart";
-        this.canvas.width = window.innerWidth * 0.75;  // 75% ширины экрана
-        this.canvas.height = 720;  // Фиксированная высота
-        this.canvas.style.justifyContent = "center";
-        this.canvas.style.alignItems = "center";
-        this.canvas.style.boxSizing = "border-box";
+        this.canvas.width = window.innerWidth * 0.75;
+        this.canvas.height = 720;
         this.canvas.style.position = "absolute";
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 
-        this.interval = setInterval(updateGameArea, 10);
+        this.interval = setInterval(window.updateGameArea, 10);
 
         this.keys = [];
-        window.addEventListener('keydown', function (e) {
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = true;
+        window.addEventListener('keydown', (e) => {
+            this.keys[e.keyCode] = true;
         });
 
-        window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = false;
+        window.addEventListener('keyup', (e) => {
+            this.keys[e.keyCode] = false;
         });
 
         this.canvas.addEventListener('wheel', function (event) {
@@ -104,123 +30,3 @@ const myGameArea = {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 };
-
-// Функция компонента
-function component(width, height, imageSrc, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.image = new Image();
-    this.image.src = imageSrc;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.gravity = 0.5;
-    this.gravitySpeed = 0;
-    this.jumpStrenght = -13;
-    this.isJumping = false;
-
-    // Обновление компонента
-    this.update = function () {
-        if (this.image.complete) {
-            myGameArea.context.drawImage(this.image, this.x, this.y, this.width, this.height);
-        }
-    };
-
-    // Перемещение компонента
-    this.newPos = function () {
-        this.x += this.speedX;
-        this.gravitySpeed += this.gravity;
-        this.y += this.speedY + this.gravitySpeed;
-    };
-
-    // Проверка на столкновение
-    this.hitSides = function () {
-        const rockbottom = myGameArea.canvas.height - this.height;
-        const rocksides = myGameArea.canvas.width - this.width;
-
-        // Проверка на выход за границы экрана
-        if (this.x > rocksides) {
-            this.x = rocksides;
-        }
-
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-            this.isJumping = false;
-        }
-
-        if (this.x < 0) {
-            this.x = 0;
-        }
-
-        // Проверка столкновений с другими компонентами
-        allComponents.forEach((comp) => {
-            if (comp !== myGamePiece) {
-                // Проверка столкновений по осям x и y
-                if (myGamePiece.x < comp.x + comp.width && myGamePiece.x + myGamePiece.width > comp.x &&
-                    myGamePiece.y < comp.y + comp.height && myGamePiece.y + myGamePiece.height > comp.y) {
-
-                    // Игрок сталкивается с объектом
-                    if (myGamePiece.y + myGamePiece.height <= comp.y + comp.height / 2) {
-                        // Если игрок стоит на объекте (падает сверху), блокируем падение
-                        myGamePiece.gravitySpeed = 0;   // Останавливаем падение
-                        myGamePiece.isJumping = false;  // Останавливаем прыжок
-                        myGamePiece.y = comp.y - myGamePiece.height;  // Устанавливаем игрока на верхнюю часть объекта
-                    } else if (myGamePiece.y + myGamePiece.height > comp.y + comp.height / 2) {
-
-                        // Если игрок находится на нижней части объекта (снизу), запрещаем прыгать
-                        if (myGamePiece.x + myGamePiece.width / 2 < comp.x + comp.width / 2) {
-                            myGamePiece.x = comp.x - myGamePiece.width;
-                        } else {
-                            // Игрок справа от объекта
-                            myGamePiece.x = comp.x + comp.width;
-                        }
-                    }
-                }
-            }
-        });
-    };
-
-    // Прыжок с эффектом
-    this.jump = function () {
-        const marioJump = new Audio("./sounds/mario-jump.mp3");
-        marioJump.volume = 0.08;
-
-        if (!this.isJumping) {
-            this.gravitySpeed = this.jumpStrenght;
-            this.isJumping = true;
-
-            if (!marioJump.paused) {
-                marioJump.currentTime = 0; // Перематываем звук на начало
-            }
-            marioJump.play();
-        }
-    };
-}
-
-// Обновление игрового экрана
-function updateGameArea() {
-    myGameArea.clear();
-
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
-
-    if (myGameArea.keys && myGameArea.keys[37]) { myGamePiece.speedX = -4; }  // Влево
-    if (myGameArea.keys && myGameArea.keys[39]) { myGamePiece.speedX = 4; }   // Вправо
-    if (myGameArea.keys && myGameArea.keys[38]) { myGamePiece.jump(); }  // Прыжок (вверх)
-    if (myGameArea.keys[37] && myGameArea.keys[39]) { myGamePiece.speedX = 0; }
-
-    // Проход по всем компонентам уровня
-    allComponents.forEach(function (comp) {
-        comp.update();
-        comp.hitSides();
-    });
-
-    myGamePiece.update();
-    myGamePiece.newPos();
-    myGamePiece.hitSides();
-}
-
-// Запуск игры
-startGame();
